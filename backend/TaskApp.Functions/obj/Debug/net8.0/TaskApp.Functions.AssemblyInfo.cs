10c60114b3b288df28b11c34bebd9700714c22bc
PhilipJohn <pjfaraon@gmail.com>
2025-09-01 11:47:46 +1000

    Implement security best practices and backend CI/CD SECURITY IMPROVEMENTS: - Remove sensitive credentials from git history - Add comprehensive .gitignore - Create template files for local development - Set up GitHub Secrets for secure credential management
    CI/CD IMPROVEMENTS:
    - Add backend deployment pipeline with GitHub Actions
    - Automated .NET build and deployment process
    - Secure Azure Functions deployment with publish profiles
    - Environment-specific configuration management

--- backend/TaskApp.Functions/obj/Debug/net8.0/TaskApp.Functions.AssemblyInfo.cs
+++ backend/TaskApp.Functions/obj/Debug/net8.0/TaskApp.Functions.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskApp.Functions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e327a35a25fcc67ea6ab59cd1da8b1d747edc970")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17c3d68827a13bb21609d2f0fedc6f0b76aecc19")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskApp.Functions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskApp.Functions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- backend/TaskApp.Functions/obj/Debug/net8.0/WorkerExtensions/obj/Debug/net6.0/WorkerExtensions.AssemblyInfo.cs
+++ backend/TaskApp.Functions/obj/Debug/net8.0/WorkerExtensions/obj/Debug/net6.0/WorkerExtensions.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Microsoft.Azure.Functions.Worker.Extensions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e327a35a25fcc67ea6ab59cd1da8b1d747edc970")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17c3d68827a13bb21609d2f0fedc6f0b76aecc19")]
 [assembly: System.Reflection.AssemblyProductAttribute("Microsoft.Azure.Functions.Worker.Extensions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Microsoft.Azure.Functions.Worker.Extensions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
